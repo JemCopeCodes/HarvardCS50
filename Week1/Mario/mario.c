dafe21a95793bfdd8cccdd2d7d9dada4eed1c411
JemCopeCodes <thejemmeh@gmail.com>
2020-11-27 22:07:01 -0800

    Finished Mario, All Tests Passed

--- Week1/Mario/mario.c
+++ Week1/Mario/mario.c
@@ -2,6 +2,24 @@
 #include <cs50.h>
 
 int main(void){
-    int height = get_int("Height: ");
+    int height = 0;
 
+    do{
+    height = get_int("Height: ");
+    }
+    while(height < 1 || height > 8);
+
+    for(int i = 1; i <= height; i++){
+        for(int x = 0; x < (height - i); x++){
+            printf(" ");
+        }
+        for(int y = 0; y < i; y++){
+            printf("#");
+        }
+        printf("  ");
+        for(int z = 0; z < i; z++){
+            printf("#");
+        }
+        printf("\n");
+    }
 }
