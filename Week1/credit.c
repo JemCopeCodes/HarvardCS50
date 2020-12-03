cc7e634c4ad7ea171eab6e9ef54cc6169fbbce24
JemCopeCodes <thejemmeh@gmail.com>
2020-12-03 12:23:09 -0800

    CC format check start

--- Week1/credit.c
+++ Week1/credit.c
@@ -17,7 +17,26 @@ Checksum
 #include <stdio.h>
 #include <cs50.h>
 
+int checkSum(long number){
+    //Luhn's Algorithm
+    printf("%ld\n", number/10 % 10);//testing getting digits
+    return 1; //temp for debug
+}
+
+int getLength(long number){
+    //13, 15, or 16 digit number?
+    printf("inside getLength\n");
+    return 13; //temp for debug
+}
+
 int main(void){
     long number = get_long("Number: ");
-    printf("AMEX\n");
+    if(checkSum(number)){
+            if(getLength(number) == 13 && number/1000000000000000 % 10 == 4){
+                printf("VISA\n");
+            }
+    } else{
+        printf("INVALID\n");
+    }
 }
+
