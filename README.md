ae1403d57ded57998dc8496ab59ce8b73bf99d16
JemCopeCodes <thejemmeh@gmail.com>
2021-03-13 23:48:07 -0800

    Substitution complete

--- README.md
+++ README.md
@@ -22,6 +22,12 @@ Calculate the reading level of text using the Coleman-Liau index.
 [Readability Problem Instructions](https://cs50.harvard.edu/x/2021/psets/2/readability/#:~:text=check50%20cs50/problems/2021/x/readability)
 
 [My Solution](https://github.com/JemCopeCodes/HarvardCS50/blob/main/Week2/readability.c)
+#### Substitution
+Takes in a key to use for a substitution cipher.
+
+[Readability Problem Instructions](https://cs50.harvard.edu/x/2021/psets/2/substitution/)
+
+[My Solution](https://github.com/JemCopeCodes/HarvardCS50/blob/main/Week2/substitution.c)
 
 ### Week 3:
 #### Plurality
--- Week2/substitution.c
+++ Week2/substitution.c
@@ -1,5 +1,13 @@
 /*
+Passes check50 cs50/problems/2021/x/substitution
 https://cs50.harvard.edu/x/2021/psets/2/substitution/
+
+Explaination of main() -- changing the ascii table index into alphabetical index.
+'a' will be converted to the ascii table number. (61)
+For example, if plainText[i] is 'h'(ascii 68) then the math of 'h' - 'a' is the same as 68 - 61 = 7
+7 is the alphabetical index of the letter h (in an array that starts from 0) because it's the 8th letter in the alphabet
+because the array starts at 0, argv[1][7] will give you the 8th letter from the key
+the alphabetical index is equal to the key index because the key is also 26 letters
 
 */
 
@@ -10,12 +18,25 @@ https://cs50.harvard.edu/x/2021/psets/2/substitution/
 
 int validateKey();
 
+
 int main(int argc, string argv[]){
     if(argc == 2 && validateKey(argv[1])){
         string plainText = get_string("plaintext: ");
-        string ciphertext = plainText;
-        // do substitution
-        printf("ciphertext: %s\n", ciphertext);
+        string cipherText = plainText;
+        int keyIndex = 0;
+
+        for(int i = 0; i < strlen(plainText); i++){
+            if(isalpha(plainText[i])){
+                if(islower(plainText[i])){
+                    keyIndex = plainText[i] - 'a';
+                    cipherText[i] = tolower(argv[1][keyIndex]);
+                } else {
+                    keyIndex = plainText[i] - 'A';
+                    cipherText[i] = toupper(argv[1][keyIndex]);
+                }
+            }
+        }
+        printf("ciphertext: %s\n", cipherText);
     } else {
         printf("Usage: ./substitution key\n");
         return 1;
